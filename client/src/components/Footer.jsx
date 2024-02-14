40f2e083327f4dfb83ac942d070076642785bd4f
Igor Carlos <igor.lima58@gmail.com>
2024-02-15 04:02:22 +0800

    sign in api/client, redux toolkit, redux persist

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { Footer } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
-export default function Footer() {
+export default function FooterCom() {
   return (
     <Footer container className='border border-t-8 border-teal-500'>
       <div className='w-full max-w-7xl mx-auto'>
