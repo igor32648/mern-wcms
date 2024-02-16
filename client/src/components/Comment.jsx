7bea183d41b4891ea4b6f02c476b7bdc3b0501cf
Igor Carlos <igor.lima58@gmail.com>
2024-02-16 22:23:15 +0800

    delete comments api/client

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
 
-export default function Comment({ comment, onLike, onEdit }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
   const [user, setUser] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [editedContent, setEditedContent] = useState(comment.content);
@@ -37,11 +37,11 @@ export default function Comment({ comment, onLike, onEdit }) {
       const res  = await fetch(`/api/comment/editComment/${comment._id}`, {
         method: 'PUT',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          content: editedContent
-        })
+          content: editedContent,
+        }),
       });
       if (res.ok) {
         setIsEditing(false);
@@ -50,7 +50,7 @@ export default function Comment({ comment, onLike, onEdit }) {
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
   return (
     <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
@@ -80,7 +80,7 @@ export default function Comment({ comment, onLike, onEdit }) {
              type='button'
              size='sm'
              gradientDuoTone='purpleToBlue'
-              onClick={handleSave}
+             onClick={handleSave}
             >
               Save
             </Button>
@@ -89,7 +89,7 @@ export default function Comment({ comment, onLike, onEdit }) {
              size='sm'
              gradientDuoTone='purpleToBlue'
              outline
-              onClick={() => setIsEditing(false)}
+             onClick={() => setIsEditing(false)}
             >
               Cancel
             </Button>
@@ -118,13 +118,22 @@ export default function Comment({ comment, onLike, onEdit }) {
               </p>
               {currentUser &&
                   (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                    <button
+                    <>
+                      <button
+                        type='button'
+                        onClick={handleEdit}
+                        className='text-gray-400 hover:text-blue-500'
+                      >
+                        Edit
+                      </button>
+                      <button
                       type='button'
-                      onClick={handleEdit}
-                      className='text-gray-400 hover:text-blue-500'
+                      onClick={() => onDelete(comment._id)}
+                      className='text-gray-400 hover:text-red-500'
                     >
-                      Edit
+                      Delete
                     </button>
+                  </>
                   )}
             </div>
           </>
