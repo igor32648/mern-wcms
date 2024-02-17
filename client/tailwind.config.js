535b451d5f1e3a553ec201d5c199e479e0a72b5d
Igor Carlos <igor.lima58@gmail.com>
2024-02-17 23:31:15 +0800

    recent posts recommendation in post page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,7 +10,8 @@ export default {
   },
   plugins: [
     require('flowbite/plugin'),
-    require('tailwind-scrollbar')
+    require('tailwind-scrollbar'),
+    require('@tailwindcss/line-clamp'),
   ],
 }
 
